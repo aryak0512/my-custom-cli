d6b7e7fe53e4e76835a18838493e8dc258b2a0a6
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-01-01 13:24:27 +0530

    Bash startup script

---
+++ executables/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -L -o cli-app-0.0.1-SNAPSHOT.jar https://raw.githubusercontent.com/aryak0512/my-custom-cli/master/executables/cli-app-0.0.1-SNAPSHOT.jar
+chmod +x cli-app-0.0.1-SNAPSHOT.jar
+java -jar cli-app-0.0.1-SNAPSHOT.jar
